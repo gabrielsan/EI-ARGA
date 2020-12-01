99bff22e32ddabb84ca2a5b9474c729d3fa38376
gabrielsan <gs4721158@gmail.com>
2020-12-01 17:54:13 -0300

    .

--- database/migrations/1603248427872_token.js
+++ database/migrations/1603248427872_token.js
@@ -7,7 +7,7 @@ class TokensSchema extends Schema {
   up () {
     this.create('tokens', (table) => {
       table.increments()
-      table.integer('id_usuario').unsigned().references('id').inTable('users')
+      table.integer('id_user').unsigned().references('id').inTable('users')
       table.string('token', 255).notNullable().unique().index()
       table.string('type', 80).notNullable()
       table.boolean('is_revoked').defaultTo(false)
